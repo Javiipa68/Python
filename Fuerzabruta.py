7fb5e142e1a4c0bac04d72b66fdf4a46b495500f
Javiipa68 <fjiparraguirreg@hotmail.com>
2024-11-13 15:17:13 +0100

    Ataque de fuerza bruta

---
+++ Fuerzabruta.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import argparse
+
+arg parser = argparse.ArgumentParser(description='Herramienta para hacer ataques de fuerza bruta')
+arg.add_argument("-u", dest="usuario", help="Pasar un Usuario", required=True)
+arg.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
